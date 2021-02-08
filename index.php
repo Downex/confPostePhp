21e6147b7ac1af35b6e1f5767ffec2f8f763eb9d
Francois CAHIER <francois.cahier@epsi.fr>
2021-02-08 16:18:07 +0100

    ajout config distance

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
-//CONF BDD
+//CONF BDD distance
 $db="confposte";
 $dbhost="localhost";
 $dbport=3306; //3307 en local
 $dbuser="francois"; // root en local
 $dbpasswd="root";
+
+// //CONF BDD distance
+// $db="confposte";
+// $dbhost="localhost";
+// $dbport=3307;
+// $dbuser="root";
+// $dbpasswd="";
  
 $pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
 $pdo->exec("SET CHARACTER SET utf8");
