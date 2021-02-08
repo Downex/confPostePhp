6f0ff459f0bcf917c6a1fc5ac5146d1e411dd672
Francois CAHIER <francois.cahier@epsi.fr>
2021-02-08 15:06:26 +0100

    mdp

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $db="confposte";
 $dbhost="localhost";
 $dbport=3307;
 $dbuser="root";
-$dbpasswd="";
+$dbpasswd="root";
  
 $pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
 $pdo->exec("SET CHARACTER SET utf8");
