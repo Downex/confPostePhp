2fc5ef80405d1e2a0e39b492507d8071c4c36e27
Francois CAHIER <francois.cahier@epsi.fr>
2021-02-08 14:59:52 +0100

    Ajout bdd serveur

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php
 //CONF BDD
-// $db="confposte";
-// $dbhost="localhost";
-// $dbport=3307;
-// $dbuser="root";
-// $dbpasswd="";
+$db="confposte";
+$dbhost="localhost";
+$dbport=3307;
+$dbuser="root";
+$dbpasswd="";
  
-// $pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
-// $pdo->exec("SET CHARACTER SET utf8");
+$pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
+$pdo->exec("SET CHARACTER SET utf8");
 
-// $stmt = $pdo->prepare("SELECT libelle FROM hello");
-// $stmt->execute();
+$stmt = $pdo->prepare("SELECT libelle FROM hello");
+$stmt->execute();
 
-// $res = $stmt->fetch();
+$res = $stmt->fetch();
 
-// Hello World
-// echo $res[0];
+//Hello World
+echo $res[0];
 
-// fermeture de la connexion
-// $pdo = null;
+//fermeture de la connexion
+$pdo = null;
 
-echo "Hello";
 ?>
